fc468f99189843f48658bc0afc01a42cb9cc450d
David J. M. Karlsen <david@davidkarlsen.com>
2016-11-15 23:05:41 +0100

    create the user

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM davidkarlsen/docker-java:8u111
 
 MAINTAINER David J. M. Karlsen <david@davidkarlsen.com>
 
-COPY entrypoint.sh /
 RUN apt-get update && \
     apt-get -y install curl daemontools git gosu && \
     apt-get clean && \
     curl --silent -L https://github.com/gliderlabs/herokuish/releases/download/v0.3.21/herokuish_0.3.21_linux_x86_64.tgz | tar -xzvC /usr/local/bin && \
     wget https://releases.hashicorp.com/envconsul/0.6.1/envconsul_0.6.1_linux_amd64.zip -O - |funzip > /usr/local/bin/envconsul && \
     chmod a+x /usr/local/bin/envconsul
+COPY entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,22 @@
 
 set -e
 
-if [ -z $GOSU_CHOWN ] && [ -e /app ]; then
-  GOSU_CHOWN="/app"
-fi
-
 # If GOSU_CHOWN environment variable set, recursively chown all specified directories 
 # to match the user:group set in GOSU_USER environment variable.
 if [ -n "$GOSU_CHOWN" ]; then
-    for DIR in $GOSU_CHOWN
-    do
-        chown -R $GOSU_USER $DIR
-    done
+  for DIR in $GOSU_CHOWN
+  do
+    chown -R $GOSU_USER $DIR
+  done
 fi  
 
 # If GOSU_USER environment variable set to something other than 0:0 (root:root),
 # become user:group set within and exec command passed in args
 if [ -n "$GOSU_USER" ] && [ "$GOSU_USER" != "0:0" ]; then
-    exec gosu $GOSU_USER "$@"
+  IFS=':' read -r -a uidgid <<< $GOSU_USER
+  groupadd -g "${uidgid[1]}" app
+  useradd -u "${uidgid[0]}" -g "${uidgid[1]}" -m -d /app -s /bin/bash app
+  exec gosu $GOSU_USER "$@"
 fi
 
 # If GOSU_USER was 0:0 exec command passed in args without gosu (assume already root)
